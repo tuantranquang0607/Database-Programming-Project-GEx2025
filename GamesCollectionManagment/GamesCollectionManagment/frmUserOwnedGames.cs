ccf134a9d9062df4ab75354a5e18a5b9ba4243e5
tuantranquang0607 <tranquangtuan0607032gmail.com>
2024-11-26 09:20:09 -0400

    This is the code for the user owned games form.

--- GamesCollectionManagment/GamesCollectionManagment/frmUserOwnedGames.cs
+++ GamesCollectionManagment/GamesCollectionManagment/frmUserOwnedGames.cs
@@ -16,5 +16,195 @@ namespace GamesCollectionManagment
         {
             InitializeComponent();
         }
+
+        private void frmUserOwnedGames_Load(object sender, EventArgs e)
+        {
+            LoadFirstGame();
+        }
+
+        int currentGameId = 0;
+        int firstGameId = 0;
+        int lastGameId = 0;
+        int? previousGameId;
+        int? nextGameId;
+
+        private void ResetToReadOnlyMode()
+        {
+            txtGameId.ReadOnly = true;
+            txtGameTitle.ReadOnly = true;
+            txtGamePublisher.ReadOnly = true;
+            txtGameReleaseDate.ReadOnly = true;
+            txtGameGenres.ReadOnly = true;
+            txtGamePlatforms.ReadOnly = true;
+        }
+
+        private void NextPreviousButtonManagement()
+        {
+            btnPrevious.Enabled = previousGameId != null;
+            btnNext.Enabled = nextGameId != null;
+        }
+
+        private void NavigationState(bool enableState)
+        {
+            btnFirst.Enabled = enableState;
+            btnLast.Enabled = enableState;
+            btnNext.Enabled = enableState;
+            btnPrevious.Enabled = enableState;
+        }
+
+        private void Navigation_Handler(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+
+            switch (b.Name)
+            {
+                case "btnFirst":
+                    currentGameId = firstGameId;
+                    break;
+
+                case "btnLast":
+                    currentGameId = lastGameId;
+                    break;
+
+                case "btnPrevious":
+                    currentGameId = previousGameId.Value;
+                    break;
+
+                case "btnNext":
+                    currentGameId = nextGameId.Value;
+                    break;
+            }
+
+            LoadGameDetails();
+            NextPreviousButtonManagement();
+        }
+
+        private void LoadGameDetails()
+        {
+            string[] sqlStatements = new string[]
+            {
+                $"SELECT * FROM GameManagment WHERE Id = {currentGameId}",
+
+                $@"
+                SELECT 
+                (
+                    SELECT TOP(1) Id as FirstGameId FROM GameManagment ORDER BY GameTitle
+                ) as FirstGameId,
+                q.PreviousGameId,
+                q.NextGameId,
+                (
+                    SELECT TOP(1) Id as LastGameId FROM GameManagment ORDER BY GameTitle Desc
+                ) as LastGameId
+                FROM
+                (
+                    SELECT Id, GameTitle,
+                    LEAD(Id) OVER(ORDER BY GameTitle) AS NextGameId,
+                    LAG(Id) OVER(ORDER BY GameTitle) AS PreviousGameId
+                    FROM GameManagment
+                ) AS q
+                WHERE q.Id = {currentGameId}
+                ORDER BY q.GameTitle"
+            };
+
+            DataSet ds = DataAccess.GetData(sqlStatements);
+
+            if (ds.Tables[0].Rows.Count == 1)
+            {
+                DataRow selectedGame = ds.Tables[0].Rows[0];
+
+                txtGameId.Text = selectedGame["Id"].ToString();
+                txtGameTitle.Text = selectedGame["GameTitle"].ToString();
+                txtGamePublisher.Text = selectedGame["GamePublisher"].ToString();
+                txtGameReleaseDate.Text = selectedGame["GameReleaseDate"] != DBNull.Value ? Convert.ToDateTime(selectedGame["GameReleaseDate"]).ToString("yyyy-MM-dd") : string.Empty;
+                txtGameGenres.Text = selectedGame["GameGenres"].ToString();
+                txtGamePlatforms.Text = selectedGame["GamePlatforms"].ToString();
+
+                firstGameId = Convert.ToInt32(ds.Tables[1].Rows[0]["FirstGameId"]);
+                previousGameId = ds.Tables[1].Rows[0]["PreviousGameId"] != DBNull.Value ? Convert.ToInt32(ds.Tables[1].Rows[0]["PreviousGameId"]) : (int?)null;
+                nextGameId = ds.Tables[1].Rows[0]["NextGameId"] != DBNull.Value ? Convert.ToInt32(ds.Tables[1].Rows[0]["NextGameId"]) : (int?)null;
+                lastGameId = Convert.ToInt32(ds.Tables[1].Rows[0]["LastGameId"]);
+            }
+            else
+            {
+                LoadFirstGame();
+            }
+        }
+
+        private void LoadFirstGame()
+        {
+            object gameId = DataAccess.GetValue("SELECT TOP (1) Id FROM GameManagment ORDER BY GameTitle");
+
+            if (gameId == null)
+            {
+                NavigationState(false);
+
+                btnCancel.Enabled = false;
+                return;
+            }
+
+            NavigationState(true);
+
+            firstGameId = Convert.ToInt32(gameId);
+            currentGameId = firstGameId;
+            LoadGameDetails();
+            NextPreviousButtonManagement();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DeleteGame();
+                ResetToReadOnlyMode();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void DeleteGame()
+        {
+            string sql = $@" DELETE FROM GameManagment WHERE Id = {currentGameId} ";
+
+            int rowsAffected = DataAccess.ExecuteNonQuery(sql);
+
+            if (rowsAffected == 1)
+            {
+                MessageBox.Show($"Game with Id: {txtGameId.Text} was deleted");
+                LoadFirstGame();
+                ResetToReadOnlyMode();
+            }
+            else if (rowsAffected == 0)
+            {
+                MessageBox.Show($"game with Id: {txtGameId.Text} does not exist. May already have been delete.");
+                LoadFirstGame();
+                ResetToReadOnlyMode();
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                LoadGameDetails();
+
+                btnDelete.Enabled = true;
+                btnSearch.Enabled = true;
+
+                NavigationState(true);
+                NextPreviousButtonManagement();
+                ResetToReadOnlyMode();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
--- GamesCollectionManagment/GamesCollectionManagment/frmUserWishlist.Designer.cs
+++ GamesCollectionManagment/GamesCollectionManagment/frmUserWishlist.Designer.cs
@@ -28,12 +28,282 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmUserWishlist";
+            label7 = new Label();
+            txtGamePublisher = new TextBox();
+            txtGameGenres = new TextBox();
+            txtGamePlatforms = new TextBox();
+            txtGameTitle = new TextBox();
+            txtGameId = new TextBox();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            txtGameReleaseDate = new TextBox();
+            label1 = new Label();
+            btnSearch = new Button();
+            btnLast = new Button();
+            btnFirst = new Button();
+            btnCancel = new Button();
+            btnDelete = new Button();
+            btnNext = new Button();
+            btnPrevious = new Button();
+            SuspendLayout();
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 18F);
+            label7.Location = new Point(37, 303);
+            label7.Name = "label7";
+            label7.Size = new Size(725, 32);
+            label7.TabIndex = 88;
+            label7.Text = "You can only add a game to your Wishlist from Game Managment.";
+            label7.Click += label7_Click;
+            // 
+            // txtGamePublisher
+            // 
+            txtGamePublisher.Location = new Point(145, 117);
+            txtGamePublisher.Name = "txtGamePublisher";
+            txtGamePublisher.ReadOnly = true;
+            txtGamePublisher.Size = new Size(446, 23);
+            txtGamePublisher.TabIndex = 87;
+            // 
+            // txtGameGenres
+            // 
+            txtGameGenres.Location = new Point(145, 213);
+            txtGameGenres.Name = "txtGameGenres";
+            txtGameGenres.ReadOnly = true;
+            txtGameGenres.Size = new Size(446, 23);
+            txtGameGenres.TabIndex = 86;
+            // 
+            // txtGamePlatforms
+            // 
+            txtGamePlatforms.Location = new Point(145, 262);
+            txtGamePlatforms.Name = "txtGamePlatforms";
+            txtGamePlatforms.ReadOnly = true;
+            txtGamePlatforms.Size = new Size(446, 23);
+            txtGamePlatforms.TabIndex = 85;
+            // 
+            // txtGameTitle
+            // 
+            txtGameTitle.Location = new Point(146, 71);
+            txtGameTitle.Name = "txtGameTitle";
+            txtGameTitle.ReadOnly = true;
+            txtGameTitle.Size = new Size(446, 23);
+            txtGameTitle.TabIndex = 84;
+            // 
+            // txtGameId
+            // 
+            txtGameId.Location = new Point(146, 22);
+            txtGameId.Name = "txtGameId";
+            txtGameId.ReadOnly = true;
+            txtGameId.Size = new Size(446, 23);
+            txtGameId.TabIndex = 83;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 15F);
+            label6.Location = new Point(12, 257);
+            label6.Margin = new Padding(4, 0, 4, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(99, 28);
+            label6.TabIndex = 82;
+            label6.Text = "Platforms:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 15F);
+            label5.Location = new Point(12, 208);
+            label5.Margin = new Padding(4, 0, 4, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(76, 28);
+            label5.TabIndex = 81;
+            label5.Text = "Genres:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 15F);
+            label4.Location = new Point(12, 160);
+            label4.Margin = new Padding(4, 0, 4, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(126, 28);
+            label4.TabIndex = 80;
+            label4.Text = "Release Date:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15F);
+            label3.Location = new Point(12, 115);
+            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(96, 28);
+            label3.TabIndex = 79;
+            label3.Text = "Publisher:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15F);
+            label2.Location = new Point(12, 66);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(53, 28);
+            label2.TabIndex = 78;
+            label2.Text = "Title:";
+            // 
+            // txtGameReleaseDate
+            // 
+            txtGameReleaseDate.Location = new Point(145, 165);
+            txtGameReleaseDate.Name = "txtGameReleaseDate";
+            txtGameReleaseDate.ReadOnly = true;
+            txtGameReleaseDate.Size = new Size(446, 23);
+            txtGameReleaseDate.TabIndex = 77;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15F);
+            label1.Location = new Point(12, 14);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(33, 28);
+            label1.TabIndex = 76;
+            label1.Text = "Id:";
+            // 
+            // btnSearch
+            // 
+            btnSearch.Location = new Point(612, 22);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(204, 45);
+            btnSearch.TabIndex = 75;
+            btnSearch.Text = "Search";
+            btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // btnLast
+            // 
+            btnLast.CausesValidation = false;
+            btnLast.Location = new Point(716, 73);
+            btnLast.Margin = new Padding(4, 3, 4, 3);
+            btnLast.Name = "btnLast";
+            btnLast.Size = new Size(100, 71);
+            btnLast.TabIndex = 74;
+            btnLast.Text = "Last";
+            btnLast.UseVisualStyleBackColor = true;
+            // 
+            // btnFirst
+            // 
+            btnFirst.CausesValidation = false;
+            btnFirst.Location = new Point(613, 73);
+            btnFirst.Margin = new Padding(4, 3, 4, 3);
+            btnFirst.Name = "btnFirst";
+            btnFirst.Size = new Size(95, 71);
+            btnFirst.TabIndex = 73;
+            btnFirst.Text = "First";
+            btnFirst.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            btnCancel.CausesValidation = false;
+            btnCancel.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Bold);
+            btnCancel.Location = new Point(716, 213);
+            btnCancel.Margin = new Padding(4, 3, 4, 3);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(100, 72);
+            btnCancel.TabIndex = 72;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            btnDelete.CausesValidation = false;
+            btnDelete.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Bold);
+            btnDelete.Location = new Point(613, 213);
+            btnDelete.Margin = new Padding(4, 3, 4, 3);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(95, 72);
+            btnDelete.TabIndex = 71;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            btnNext.CausesValidation = false;
+            btnNext.Location = new Point(716, 150);
+            btnNext.Margin = new Padding(4, 3, 4, 3);
+            btnNext.Name = "btnNext";
+            btnNext.Size = new Size(100, 57);
+            btnNext.TabIndex = 70;
+            btnNext.Text = "Next";
+            btnNext.UseVisualStyleBackColor = true;
+            // 
+            // btnPrevious
+            // 
+            btnPrevious.CausesValidation = false;
+            btnPrevious.Location = new Point(613, 150);
+            btnPrevious.Margin = new Padding(4, 3, 4, 3);
+            btnPrevious.Name = "btnPrevious";
+            btnPrevious.Size = new Size(95, 57);
+            btnPrevious.TabIndex = 69;
+            btnPrevious.Text = "Previous";
+            btnPrevious.UseVisualStyleBackColor = true;
+            // 
+            // frmUserWishlist
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(831, 345);
+            Controls.Add(label7);
+            Controls.Add(txtGamePublisher);
+            Controls.Add(txtGameGenres);
+            Controls.Add(txtGamePlatforms);
+            Controls.Add(txtGameTitle);
+            Controls.Add(txtGameId);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(txtGameReleaseDate);
+            Controls.Add(label1);
+            Controls.Add(btnSearch);
+            Controls.Add(btnLast);
+            Controls.Add(btnFirst);
+            Controls.Add(btnCancel);
+            Controls.Add(btnDelete);
+            Controls.Add(btnNext);
+            Controls.Add(btnPrevious);
+            Name = "frmUserWishlist";
+            Text = "frmUserWishlist";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label7;
+        private TextBox txtGamePublisher;
+        private TextBox txtGameGenres;
+        private TextBox txtGamePlatforms;
+        private TextBox txtGameTitle;
+        private TextBox txtGameId;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private TextBox txtGameReleaseDate;
+        private Label label1;
+        private Button btnSearch;
+        private Button btnLast;
+        private Button btnFirst;
+        private Button btnCancel;
+        private Button btnDelete;
+        private Button btnNext;
+        private Button btnPrevious;
     }
 }
